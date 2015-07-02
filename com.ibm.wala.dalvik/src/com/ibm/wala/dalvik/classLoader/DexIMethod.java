6a8bcdacfb956a72cfadcb98e450dee48c4884d1
Michael Herzberg <michael.herzberg@sap.com>
2015-07-02 20:37:51 +0200

    Added possibility to add dex line number information manually.

--- com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/classLoader/DexIMethod.java
+++ com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/classLoader/DexIMethod.java
@@ -61,6 +61,7 @@ import static org.jf.dexlib.Util.AccessFlags.VOLATILE;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -180,8 +181,10 @@ public class DexIMethod implements IBytecodeMethod {
 	private TypeReference typeReference;
 
 	private ExceptionHandler[][] handlers;
-	
+
 	protected InstructionArray instructions;
+
+	private Map<Integer, Integer> lineNumbers = new HashMap<Integer, Integer>();
 
 
 
@@ -537,7 +540,12 @@ public class DexIMethod implements IBytecodeMethod {
 	}
 
 	public int getLineNumber(int bcIndex) {
-		return getInstructionIndex(bcIndex);
+//		return getInstructionIndex(bcIndex);
+		return lineNumbers.getOrDefault(bcIndex, -1);
+	}
+
+	public void setLineNumber(int bcIndex, int lineNumber) {
+		lineNumbers.put(bcIndex, lineNumber);
 	}
 
 	/*
