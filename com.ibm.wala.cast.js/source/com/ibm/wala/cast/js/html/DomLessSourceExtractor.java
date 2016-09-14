0bcbe4359631d6089181341418373a9547e5c05b
Michael Herzberg <msherzberg1@sheffield.ac.uk>
2016-09-14 16:28:28 +0100

    Reverted visibility of private method.

--- com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/html/DomLessSourceExtractor.java
+++ com.ibm.wala.cast.js/source/com/ibm/wala/cast/js/html/DomLessSourceExtractor.java
@@ -249,7 +249,7 @@ public class DomLessSourceExtractor extends JSSourceExtractor {
       }
     }
 
-    protected void getScriptFromUrl(String urlAsString, ITag scriptTag) throws IOException, MalformedURLException {
+    private void getScriptFromUrl(String urlAsString, ITag scriptTag) throws IOException, MalformedURLException {
       URL scriptSrc = new URL(entrypointUrl, urlAsString);
       Reader scriptInputStream;
       try {
